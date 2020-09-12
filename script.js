fc23f52b2717616803d83d597824261750bf1273
Irina <janneteldenorr@gmail.com>
2020-09-12 15:11:39 +0700

    Exercise 2.Lesson 2.10. 1st part of project

--- script.js
+++ script.js
@@ -12,19 +12,67 @@ let appData = {
     savings: false,
 };
 
-let questionFirst = prompt(
-        "Введите обязательную статью расходов в этом месяце", ""),
-    questionSecond = prompt(
-        "Введите обязательную статью расходов в этом месяце", "");
+let questionFirst = "",
+    questionSecond = ""; 
 
-appData.necessaryExpenses[questionFirst] = prompt(
-    "Во сколько обойдется", "");
-appData.necessaryExpenses[questionSecond] = prompt(
-    "Во сколько обойдется", "");
+// for (let i = 0; i < 2; i++) {
+//     questionFirst = prompt("Введите обязательную статью расходов в этом месяце", "");
+//     questionSecond = +prompt("Во сколько обойдется", "");
 
-alert((+appData.budget/30).toFixed(2));
+//     if (typeof(questionFirst) === "string" && typeof(questionFirst) != null 
+//         && typeof(questionSecond) != null && questionFirst != '' 
+//         && questionSecond != '' && questionFirst.length < 50) {
 
-console.log(appData.budget + " " + appData.timeData + " " + 
-    appData.necessaryExpenses[questionFirst]);
+//         appData.necessaryExpenses[questionFirst] = questionSecond;
+//     }else { 
+    //     i--;
+    // }
+
+// }
+let i = 0;
+// while (i < 2) {
+//     questionFirst = prompt("Введите обязательную статью расходов в этом месяце", "");
+//     questionSecond = +prompt("Во сколько обойдется", "");
+
+//     if (typeof(questionFirst) === "string" && typeof(questionFirst) != null 
+//         && typeof(questionSecond) != null && questionFirst != '' 
+//         && questionSecond != '' && questionFirst.length < 50) {
+
+//         appData.necessaryExpenses[questionFirst] = questionSecond;
+//     } else { 
+    //     i--;
+    // }
+
+//     i++;
+// }
+do {
+    questionFirst = prompt("Введите обязательную статью расходов в этом месяце", "");
+    questionSecond = +prompt("Во сколько обойдется", "");
+
+    if (typeof(questionFirst) === "string" && typeof(questionFirst) != null 
+        && typeof(questionSecond) != null && questionFirst != '' 
+        && questionSecond != '' && questionFirst.length < 50) {
+
+        appData.necessaryExpenses[questionFirst] = questionSecond;
+    } else { 
+        i--;
+    }
+
+    i++;
+} while (i < 2);
+
+appData.moneyPerDay = (+appData.budget/30).toFixed(2);
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay >= 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Что-то пошло не так");
+}
 
 
